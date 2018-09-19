e3eb735c585d3a292926e41d2c678832e3035a55
patel-km <karipatel95@hotmail.com>
2018-09-19 17:51:05 -0400

    routes working, not connected with db yet

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/partials/Header";
 import Welcome from "./components/pages/Welcome";
+import Collection from  "./components/pages/Collection";
+import PageNotFound from  "./components/pages/PageNotFound";
 
 
 const App =() => (
@@ -10,6 +12,8 @@ const App =() => (
     <div>
       <Header />
       <Route exact path="/" component={ Welcome } />
+      <Route exact path="/collections/:id" render={ props => <Collection {...props} /> } />
+      <Route exact path="/404" component= {PageNotFound} />
     </div>
   </Router>
 
