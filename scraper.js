892e3f89dc5244e48162c981538eb86ebc6f38d8
Mitchell <MitchellBrooks@gmail.com>
2018-09-19 21:51:24 -0400

    tweaks

--- scraper.js
+++ scraper.js
@@ -5,12 +5,8 @@ axios.get("https://www.youtube.com/watch?v=NtQkz0aRDe8")
 
     .then(response => {
       console.log(response.data);
-      // console.log(behanceResponse);
-      // console.log(dribbbleResponse);
-      // let body = ""
       let $ = cheerio.load(response.data);
       let resultsArr = [];
       let body = $("body");
-      // body = dribbbleResponse.data;
-      // console.log(body);
+
     });
