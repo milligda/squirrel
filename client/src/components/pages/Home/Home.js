0e65a52e6bef8fcdc00c163f74a30706010e3e61
DMilligan <dmilligan@springlocker.com>
2018-09-21 14:11:20 -0400

    fixing redirect bugs when logged in / not logged in

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import API from "../../../utils/API";
 
 class Home extends Component {
+  state = {
+    loggedIn: true,
+    userId: null
+  };
 
-    render() {
-        return (
-            <div>
-                <h1>Welcome to your home page!</h1>
-            </div>
-        );
+  componentDidMount = () => {
+    this.getUser();
+  };
+
+  getUser = () => {
+    API.getUserStatus()
+      .then(res => {
+        console.log(res);
+        this.setState({
+          loggedIn: res.data.loggedIn,
+          userId: res.data.userId
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    if (!this.state.loggedIn) {
+      return <Redirect to="/" />;
     }
+
+    return (
+      <div>
+        <h1>Welcome to your home page!</h1>
+      </div>
+    );
+  }
 }
 
 export default Home;
