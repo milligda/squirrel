3ab608bf9c751c7760d68e0707fb3b0a025e2e28
Mitchell <MitchellBrooks@gmail.com>
2018-09-23 23:28:06 -0400

    progress

--- dbTest.js
+++ dbTest.js
@@ -9,79 +9,75 @@ mongoose.connect('mongodb://localhost/squirrel');
 
 var db = require("./models");
 
-// db.User.create({
-//         userNameFirst: "Emily",
-//         userNameLast: "Mckenna",
-//         username: "emilymckenna08@gmail.com",
-//         password: "test",
-//         lastLogin: 08 / 26 / 1992,
-//         status: "Active",
-//     }, {
-//         userNameFirst: "Mitchell",
-//         userNameLast: "Brooks",
-//         username: "mitchellbrooks@gmail.com",
-//         password: "test2",
-//         lastLogin: 11 / 26 / 1989,
-//         status: "Active",
-//     }, )
-//     .then(function (dbUser) {
-//         console.log(dbUser);
-//     })
-//     .catch(function (err) {
-//         console.log(err.message);
-//     });
+db.User.create({
+    email: "emilymckenna08@gmail.com",
+    password: "test",
+    lastLogin: 08 / 26 / 1992,
+    status: "Active",
+  }, {
+    email: "mitchellbrooks@gmail.com",
+    password: "test2",
+    lastLogin: 11 / 26 / 1989,
+    status: "Active",
+  }, )
+  .then(function (dbUser) {
+    console.log(dbUser);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  });
 
 db.Playlist.create({
-        userId: 1,
-        description: "Makeup Tutorials",
-        title: "My Makeup Tuts",
-        private: true,
-        videos: []
-    }, {
-        userId: 1,
-        description: "Videos of Squirrels being Squirrely",
-        title: "My Squirrel Vids",
-        private: false,
-        videos: []
-    }, {
-        userId: 1,
-        description: "Recipes",
-        title: "My Cooking Videos",
-        private: false,
-        videos: []
-    })
-    .then(function (dbPlaylist) {
-        console.log(dbPlaylist);
-    })
-    .catch(function (err) {
-        console.log(err.message);
-    });
+    userId: 1,
+    description: "Makeup Tutorials",
+    title: "My Makeup Tuts",
+    private: true,
+    videos: []
+  }, {
+    userId: 1,
+    description: "Videos of Squirrels being Squirrely",
+    title: "My Squirrel Vids",
+    private: false,
+    videos: []
+  }, {
+    userId: 1,
+    description: "Recipes",
+    title: "My Cooking Videos",
+    private: false,
+    videos: []
+  })
+  .then(function (dbPlaylist) {
+    console.log(dbPlaylist);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  });
 
 db.Video.create({
-        url: "https://www.youtube.com/watch?v=4yikpWtIFU8",
-        videoPlatform: "Youtube",
-        title: "10 Funniest Squirrel Videos",
-        videoId: "4yikpWtIFU8"
-    }, {
-        url: "https://www.youtube.com/watch?v=IDaqFiLvcB0",
-        videoPlatform: "Youtube",
-        title: "Squirrels for Pets",
-        videoId: "IDaqFiLvcB0"
-    }, {
-        url: "https://www.youtube.com/watch?v=n9fKPPcZyS8",
-        videoPlatform: "Youtube",
-        title: "Squirrels being funnier than dogs?",
-        videoId: "n9fKPPcZyS8"
-    }, {
-        url: "https://www.youtube.com/watch?v=h6RWcDHA-h4",
-        videoPlatform: "Youtube",
-        title: "Rude Squirrels",
-        videoId: "h6RWcDHA-h4"
-    })
+    url: "https://www.youtube.com/watch?v=4yikpWtIFU8",
+    videoPlatform: "Youtube",
+    title: "10 Funniest Squirrel Videos",
+    videoId: "4yikpWtIFU8"
+  }, {
+    url: "https://www.youtube.com/watch?v=IDaqFiLvcB0",
+    videoPlatform: "Youtube",
+    title: "Squirrels for Pets",
+    videoId: "IDaqFiLvcB0"
+  }, {
+    url: "https://www.youtube.com/watch?v=n9fKPPcZyS8",
+    videoPlatform: "Youtube",
+    title: "Squirrels being funnier than dogs?",
+    videoId: "n9fKPPcZyS8"
+  }, {
+    url: "https://www.youtube.com/watch?v=h6RWcDHA-h4",
+    videoPlatform: "Youtube",
+    title: "Rude Squirrels",
+    videoId: "h6RWcDHA-h4"
+  })
 
-    .then(function (dbVideos) {
-        console.log(dbVideos);
-    })
-    .catch(function (err) {
-        console.log(err.message);
-    });
+  .then(function (dbVideos) {
+    console.log(dbVideos);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  });
