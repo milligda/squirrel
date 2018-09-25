e1dc6a8f233e233ade74712bd2ffd21c7e9a25d2
Mitchell <MitchellBrooks@gmail.com>
2018-09-25 00:26:12 -0400

    incremental commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,46 +44,6 @@ module.exports = {
     res.json(statusObj);
   },
 
-<<<<<<< HEAD
-  cookie: function(req, res) {
-    // check if req.user exists and store the userId
-    if (req.user) {
-      // set cookie
-      const cookieObj = {
-        userId: req.user._id
-      };
-      res.cookie(req.user._id, req.user._id, { maxAge: 2592000000 });  // Expires in one month    
-      res.json(cookieObj);
-  } else {
-    res.json({
-      error: `Sorry, there has been an error.`
-    });
-  }
-},
-  // user signup process
-  create: function(req, res) {
-    // store the username and password from req.body
-    const { username, password } = req.body;
-
-    // First - verify that the username does not already exist
-    // Then - create the user if the email does not already exist
-    db.User.findOne({ username: username }, (err, user) => {
-      if (err) {
-        console.log(err);
-      } else if (user) {
-        res.json({
-          error: `Sorry, there is already a user with email: ${username}`
-        })
-      } else {
-        db.User.create(req.body)
-        .then(dbResponse => res.json(dbResponse))
-        .catch(err => res.status(422).json(err));
-      }
-    });
-  },
-
-=======
->>>>>>> master
   // user logout process
   logout: function(req, res) {
     console.log("******** Logout called ********")
--- extensions/chrome/js/popup.js
+++ extensions/chrome/js/popup.js
@@ -1,171 +1,155 @@
-  sq = {};
-  var userId = "";
-  sq.authenticate = function () {
-    if (!localStorage.getItem("userId")) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', 'http://127.0.0.1:3000/api/users/login/external', true);
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-          // Typical action to be performed when the document is ready:
-          userId = JSON.parse(xhr.responseText).userId;
-          localStorage.setItem("userId", userId);
-          sq.urlSend();
-        } else {
-          // error result
-          document.getElementById("mainText").innerHTML = "<p>Error: " + xhr.responseText + "</p>";
-
-        }
-      };
-      username = "neue@new.com";
-      password = "testPassword";
-      authObj = {
-        "username": username,
-        "password": password
-      };
-
-      xhr.send(JSON.stringify(authObj));
-      document.getElementById("mainText").innerHTML = `logging in`;
-    }
-   else {
-    userId = localStorage.getItem("userId")
-    sq.urlSend();
-   }
-   }
-
-  sq.urlSend = function () {
-    console.log("hello");
-    var url = "";
-    var terms = [];
-    var id = "";
-    var reqObj = {};
-    var title = "";
-
-    chrome.tabs.query({
-      'active': true,
-      'lastFocusedWindow': true
-    }, function (tabs) {
-
-      var url = document.createElement('a');
-      url.href = tabs[0].url;
-
-      if (url.hostname.indexOf("youtube") != -1) {
-        vP = "youtube";
-        title = tabs[0].title.split(" - YouTube")[0];
-        src = tabs[0].url;
-        id = url.search.split("=")[1];
-      } else if (url.hostname.indexOf("vimeo") != -1) {
-        vP = "vimeo";
-        title = tabs[0].title.split(" on Vimeo")[0];
-        src = tabs[0].url;
-        id = url.pathname.split("/")[url.pathname.split("/").length - 1]
-      } else {
-        return "This website isn't supported yet."
-      }
-
-      vidObj = {
-        videoPlatform: vP,
-        title: title,
-        url: src,
-        videoId: id
-      };
-      // document.getElementById("mainText").innerHTML = "<p>" + JSON.stringify(vidObj) + "</p>";
-
-      var xhr2 = new XMLHttpRequest();
-      xhr2.open('POST', 'http://127.0.0.1:3000/api/videos/external', true);
-      xhr2.setRequestHeader('Content-Type', 'application/json');
-      xhr2.onreadystatechange = function () {
-        if (xhr2.readyState == 4 && xhr2.status == 200) {
-          // Typical action to be performed when the document is ready:
-          console.log(xhr2.responseText);
-          sq.addUserVid(xhr2.responseText);
-        } else {
-          // error result
-          document.getElementById("mainText").innerHTML = "<p>Error: " + xhr2.responseText + "</p>";
-
-        }
-        document.getElementById("mainText").innerHTML = "<p>" + xhr2.responseText + "</p>";
-
-      };
-      xhr2.send(JSON.stringify(vidObj));
-
-
-    });
-  };
-
-  sq.addUserVid = function (xres) {
-    vidId = JSON.parse(xhrresp)._id;
-    userVidObj = {
-      userId : userId,
-      vidId : vidId
-    };
-    var xhr3 = new XMLHttpRequest();
-    xhr3.open('PUT', 'http://127.0.0.1:3000/api/videos/external', true);
-    xhr3.setRequestHeader('Content-Type', 'application/json');
-    xhr3.onreadystatechange = function () {
-      if (xhr3.readyState == 4 && xhr3.status == 200) {
+var domain = 'http://127.0.0.1:3000';
+sq = {};
+var userId = "";
+sq.authenticate = function () {
+  if (!localStorage.getItem("userId")) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', `${domain}/api/users/login/external`, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
         // Typical action to be performed when the document is ready:
-        document.getElementById("mainText").innerHTML = "<p>Saved to your videos</p>";
-
+        userId = JSON.parse(xhr.responseText).userId;
+        localStorage.setItem("userId", userId);
+        sq.urlSend();
       } else {
         // error result
-        document.getElementById("mainText").innerHTML = "<p>Error:" + xhr3.responseText + "</p>";
+        document.getElementById("mainText").innerHTML = "<p>Error: " + xhr.responseText + "</p>";
 
       }
+    };
+    username = "neue@new.com";
+    password = "testPassword";
+    authObj = {
+      "username": username,
+      "password": password
+    };
+
+    xhr.send(JSON.stringify(authObj));
+    document.getElementById("mainText").innerHTML = `logging in`;
+  } else {
+    userId = localStorage.getItem("userId")
+    sq.urlSend();
+  }
+}
+
+sq.urlSend = function () {
+  console.log("hello");
+  var url = "";
+  var terms = [];
+  var id = "";
+  var reqObj = {};
+  var title = "";
+
+  chrome.tabs.query({
+    'active': true,
+    'lastFocusedWindow': true
+  }, function (tabs) {
+
+    var url = document.createElement('a');
+    url.href = tabs[0].url;
+
+    if (url.hostname.indexOf("youtube") != -1) {
+      vP = "youtube";
+      title = tabs[0].title.split(" - YouTube")[0];
+      src = tabs[0].url;
+      id = url.search.split("=")[1];
+    } else if (url.hostname.indexOf("vimeo") != -1) {
+      vP = "vimeo";
+      title = tabs[0].title.split(" on Vimeo")[0];
+      src = tabs[0].url;
+      id = url.pathname.split("/")[url.pathname.split("/").length - 1]
+    } else {
+      return "This website isn't supported yet."
+    }
+
+    vidObj = {
+      videoPlatform: vP,
+      title: title,
+      url: src,
+      videoId: id
+    };
+    // document.getElementById("mainText").innerHTML = "<p>" + JSON.stringify(vidObj) + "</p>";
+
+    var xhr2 = new XMLHttpRequest();
+    xhr2.open('POST', `${domain}/api/videos/external`, true);
+    xhr2.setRequestHeader('Content-Type', 'application/json');
+    xhr2.onreadystatechange = function () {
+      if (xhr2.readyState == 4 && xhr2.status == 200) {
+        // Typical action to be performed when the document is ready:
+        console.log(xhr2.responseText);
+        sq.addUserVid(xhr2.responseText);
+      } else {
+        // error result
+        document.getElementById("mainText").innerHTML = "<p>Error: " + xhr2.responseText + "</p>";
+
+      }
+      document.getElementById("mainText").innerHTML = "<p>" + xhr2.responseText + "</p>";
 
     };
-    console.log("sending: " + JSON.stringify(userVidObj));
-    xhr3.send(JSON.stringify(userVidObj));
+    xhr2.send(JSON.stringify(vidObj));
 
 
+  });
+};
 
+sq.addUserVid = function (xres) {
+  vidId = JSON.parse(xhrresp)._id;
+  userVidObj = {
+    userId: userId,
+    vidId: vidId
+  };
+  var xhr3 = new XMLHttpRequest();
+  xhr3.open('PUT', `${domain}/api/videos/external`, true);
+  xhr3.setRequestHeader('Content-Type', 'application/json');
+  xhr3.onreadystatechange = function () {
+    if (xhr3.readyState == 4 && xhr3.status == 200) {
+      // Typical action to be performed when the document is ready:
+      document.getElementById("mainText").innerHTML = "<p>Saved to your videos</p>";
+
+    } else {
+      // error result
+      document.getElementById("mainText").innerHTML = "<p>Error:" + xhr3.responseText + "</p>";
+
+    }
 
   };
+  console.log("sending: " + JSON.stringify(userVidObj));
+  xhr3.send(JSON.stringify(userVidObj));
 
-  // sq.getCollection = function () {
-  //   // Request from server to get collection information based on userID
-  //   var xhr = new XMLHttpRequest();
-  //   xhr.open('POST', 'http://127.0.0.1:3000/api/users/external/login', true);
-  //   xhr.setRequestHeader('Content-Type', 'application/json');
-  //   xhr.onreadystatechange = function () {
-  //     if (xhr.readyState == 4 && xhr.status == 200) {
-  //       // Typical action to be performed when the document is ready:
 
-  //       document.getElementById("mainText").innerHTML = "<p>Collections: " + JSON.stringify(xhr.responseText) + "</p>";
-  //     } else {
-  //       // error result
-  //       document.getElementById("mainText").innerHTML = "<p>Response:" + xhr.responseText + "</p>";
 
-  //     }
-  //   };
-  //   xhr.send('{"username":"neue@new.com","password":"testPassword"}');
-  // };
 
-  window.onload = function () {
-    sq.authenticate();
-    // sq.urlSend();
+};
+
+sq.getPlaylists = function () {
+  //Request from server to get playlist information based on userID
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', `${domain}/api/playlists/user/${userId}`, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      // Typical action to be performed when the document is ready:
+
+      document.getElementById("mainText").innerHTML = "<p>Playlists: " + JSON.stringify(xhr.responseText) + "</p>";
+    } else {
+      // error result
+      document.getElementById("mainText").innerHTML = "<p>Response:" + xhr.responseText + "</p>";
+
+    }
   };
+};
 
-
-  // document.getElementById("mainText").innerHTML = "<p>To be replaced</p>";
-  // //Request from server to get playlist information based on userID
-  // var xhr = new XMLHttpRequest();
-  // xhr.open('GET', 'http://127.0.0.1:3000/api/playlists/user/1', true);
-  // xhr.setRequestHeader('Content-Type','application/json');
-  // xhr.onreadystatechange = function() {
-  //   if (xhr.readyState == 4 && xhr.status == 200) {
-  //      // Typical action to be performed when the document is ready:
-
-  //     document.getElementById("mainText").innerHTML = "<p>Playlists: " + JSON.stringify(xhr.responseText) + "</p>";
-  //   } else {
-  //     // error result
-  //     document.getElementById("mainText").innerHTML = "<p>Response:" + xhr.responseText + "</p>";
+window.onload = function () {
+  sq.authenticate();
+};
 
 
 
-  // // Receive response with mongo-generated ID
 
-  // // New xhr to add video ID to selected playlist
+// // Receive response with mongo-generated ID
+
+// // New xhr to add video ID to selected playlist
 
 
-  // });
+// });
