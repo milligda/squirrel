ca3745b333d68e649e16269ff1c727b8febee778
patel-km <karipatel95@hotmail.com>
2018-09-28 18:37:00 -0400

    added vectors

--- client/src/components/partials/Controls/EditButton.js
+++ client/src/components/partials/Controls/EditButton.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./controls.css";
 
 const EditButton = () => {
-
+    <div className="edit-btn">
+        <img src="../../assets/images/edit_btn.svg" />
+    </div>  
 }
 
 export default EditButton;
--- client/src/components/partials/ReorderMenu/ReorderMenu.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ReorderMenu = (
-    <div>
-        {/* List videos on page and reorder drag and drop. Library? */}
-    </div>
-);
-
-export default ReorderMenu;
