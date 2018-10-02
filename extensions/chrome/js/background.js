78d2ba4465aa6f805b412510521c765224c37869
Mitchell <MitchellBrooks@gmail.com>
2018-10-02 18:55:04 -0400

    styling and preparation for chrome store

--- extensions/chrome/js/background.js
+++ extensions/chrome/js/background.js
@@ -1,25 +1,39 @@
-chrome.browserAction.setBadgeBackgroundColor({color:[190, 190, 190, 230]});
-chrome.browserAction.setBadgeText({text:""});
+chrome.browserAction.setBadgeBackgroundColor({
+  color: [190, 190, 190, 230]
+});
+chrome.browserAction.setBadgeText({
+  text: ""
+});
+
+compSites = ["/youtube/", "/vimeo/"]
 
 
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.url.match(/youtube/)) {
-      chrome.browserAction.setIcon({path:"../img/icon_tailx38.png"});
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if ((changeInfo.url.match(/youtube/)) || (changeInfo.url.match(/vimeo/))) {
+    chrome.browserAction.setIcon({
+      path: "../img/icon_tailx38.png"
+    });
+  } else {
+    chrome.browserAction.setIcon({
+      path: "../img/icon_tail_grayx38.png"
+    });
+  }
+
+});
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+  // how to fetch tab url using activeInfo.tabid
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
+    if ((tab.url.match(/youtube/)) || (tab.url.match(/vimeo/))) {
+      chrome.browserAction.setIcon({
+        path: "../img/icon_tailx38.png"
+      });
     } else {
-      chrome.browserAction.setIcon({path:"../img/icon_tail_grayx38.png"})
+      chrome.browserAction.setIcon({
+        path: "../img/icon_tail_grayx38.png"
+      });
     }
 
- }); 
- 
- chrome.tabs.onActivated.addListener(function(activeInfo) {
-   // how to fetch tab url using activeInfo.tabid
-   chrome.tabs.get(activeInfo.tabId, function(tab){
-      if (tab.url.match(/youtube/)) {
-      chrome.browserAction.setIcon({path:"../img/icon_tailx38.png"});
-    } else {
-      chrome.browserAction.setIcon({path:"../img/icon_tail_grayx38.png"})
-    }
-
-   });
- }); 
+  });
+});
 // chrome.browserAction.setIcon({path: icon});
