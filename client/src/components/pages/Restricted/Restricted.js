667ea6940d938136feb5d08069c9b622d04f9bc8
DMilligan <dmilligan@springlocker.com>
2018-10-03 09:22:19 -0400

    formatting nits

--- client/src/components/pages/Restricted/Restricted.js
+++ client/src/components/pages/Restricted/Restricted.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import Header from "../../partials/Header"
+import "./restricted.css";
 
 class Restricted extends Component {
     render() {
         return (
-            <div className="restricted-container">
+
+            <div className="restricted-page">
                 < Header />
-                <div className="msg container">
+                <div className="page-container restricted-container">   
                     <h1>Restricted</h1>
+                    <h2>Unfortunately you do not have access to this page.</h2>
                 </div>
             </div>
+            
         );
     }
 }
