0c572907fa66f3138f9b1e7e83ce66aceac89a0c
Mitchell <MitchellBrooks@gmail.com>
2018-10-04 15:47:20 -0400

    final tweaks

--- extensions/chrome/js/background.js
+++ extensions/chrome/js/background.js
@@ -1,15 +1,5 @@
-chrome.browserAction.setBadgeBackgroundColor({
-  color: [190, 190, 190, 230]
-});
-chrome.browserAction.setBadgeText({
-  text: ""
-});
-
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-
-
-  if (changeInfo.url.match(".*youtube\.com\/watch.*|.*vimeo\.com\/.*[0-9]+.*|.*nytimes\.com\/video\/.*|.*ted.com\/talks\/.+"))  {
+  if (changeInfo.url.match(".*youtube\.com\/watch.*|.*vimeo\.com\/.*[0-9]+.*|.*nytimes\.com\/video\/.*|.*ted.com\/talks\/.+")) {
     chrome.browserAction.setIcon({
       path: "../img/icon_acorn38px.png"
     });
@@ -18,7 +8,6 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
       path: "../img/icon_acorn_gray38px.png"
     });
   }
-
 });
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
@@ -36,20 +25,20 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
   });
 });
 
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-  if(details.frameId === 0) {
-      // Fires only when details.url === currentTab.url
-      chrome.tabs.get(details.tabId, function(tab) {
-            if (tab.url.match(".*youtube\.com\/watch.*|.*vimeo\.com\/.*[0-9]+*.*|.*nytimes\.com\/video\/.*|.*ted.com\/talks\/.+")) {
-              chrome.browserAction.setIcon({
-                path: "../img/icon_acorn38px.png"
-              });
-            } else {
-              chrome.browserAction.setIcon({
-                path: "../img/icon_tail_gray38px.png"
-              });
-            }
-      });
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
+  if (details.frameId === 0) {
+    // Fires only when details.url === currentTab.url
+    chrome.tabs.get(details.tabId, function (tab) {
+      if (tab.url.match(".*youtube\.com\/watch.*|.*vimeo\.com\/.*[0-9]+.*|.*nytimes\.com\/video\/.*|.*ted.com\/talks\/.+")) {
+        chrome.browserAction.setIcon({
+          path: "../img/icon_acorn38px.png"
+        });
+      } else {
+        chrome.browserAction.setIcon({
+          path: "../img/icon_tail_gray38px.png"
+        });
+      }
+    });
   }
 });
 // chrome.browserAction.setIcon({path: icon});
